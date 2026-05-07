04a679884e34ae76c23e9b0a285ae837a88d3b04
muhammad-hashir-butt <hashirbutt303@gmail.com>
2026-05-07 14:25:54 +0500

    .

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,5 @@
 import Hero from '@/components/Home/Hero'
-<<<<<<< HEAD
-=======
 import Services from '@/components/Home/Services'
->>>>>>> 6bfe6a64e2fcaa6b2e0ed0a69d7f6ec8499f96ed
 import About from '@/components/Home/About'
 import Packages from '@/components/Home/Packages'
 import Reviews from '@/components/Home/Reviews'
@@ -12,10 +9,7 @@ const Main = () => {
     <div>
         <Hero />
         <About />
-<<<<<<< HEAD
-=======
         <Services />
->>>>>>> 6bfe6a64e2fcaa6b2e0ed0a69d7f6ec8499f96ed
         <Packages />
         <Reviews />
     </div>
